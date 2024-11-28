bd2a5b501473069e8f38450a6d0d6add42a44834
Isabelpat <pibrisabel@gmail.com>
2024-11-27 20:37:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+  // Selecciona todos los enlaces del menú
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  // Agrega un evento de clic a cada enlace
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      // Elimina la clase 'active' de todos los enlaces
+      navLinks.forEach(link => link.classList.remove('active', 'bg-success'));
+      
+      // Agrega la clase 'active' y 'bg-success' solo al enlace actual
+      this.classList.add('active', 'bg-success');
+    });
+  });
+
+
+
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
@@ -13,3 +29,12 @@ typewriter.typeString('<span style="color: #3498db;">Ingeniera bioquímica</span
     .typeString('<span style="color: #e74c3c;"><strong>Científica de datos</strong></span>') // Rojo
     .pauseFor(2500)
     .start();
+
+
+
+    
+
+
+
+
+
